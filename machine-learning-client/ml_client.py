42fde972729faf1765022e8f0f2467cb7c40004d
Nmeng01 <ndm9914@nyu.edu>
2024-04-07 20:22:02 -0400

    pass tests

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -1,13 +1,16 @@
+"""Module to get AI generated recommendations"""
+
+# import os
 import openai
-import os
 
-openai.my_api_key = "temp" # api key goes here
+openai.my_api_key = "temp"  # api key goes here
 
-location = "New York City"
-messages = [ {"role": "system", "content":"You are an intelligent assistant."},
-            {"role": "user", "content":"List the 5 best things to do in " + location} ]
+LOCATION = "New York City"
+messages = [
+    {"role": "system", "content": "You are an intelligent assistant."},
+    {"role": "user", "content": "List the 5 best things to do in " + LOCATION},
+]
 chat = openai.chat.completions.create(messages=messages, model="gpt-3.5-turbo")
 reply = chat.choices[0].message.content
 print(reply)
-messages.append({"role": "assistant", "content": reply}) 
-
+messages.append({"role": "assistant", "content": reply})
