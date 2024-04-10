a9a4c0143a3f3e6aabd44994bce2966e6a111207
Nmeng01 <ndm9914@nyu.edu>
2024-04-10 02:26:47 -0400

    add one more test for ~80%

--- tests/test_ml_client.py
+++ tests/test_ml_client.py
@@ -1,7 +1,8 @@
 import os
 import pytest
 from mongomock import MongoClient
-from machineLearningClient.app import create_app
+from unittest.mock import patch, MagicMock
+from machineLearningClient.app import create_app, predict
 
 
 def test_sanity_check():
@@ -9,6 +10,17 @@ def test_sanity_check():
     actual = True  # the value we see in reality
     assert actual == expected, "Expected True to be equal to True!"
 
+def test_predict():
+    with patch('openai.chat.completions.create') as mock_create:
+        mock_completion = MagicMock()
+        mock_create.return_value = mock_completion
+        location = "New York New York United States of America"
+        predict(location, "mock key")
+        mock_create.assert_called_once_with(
+            messages=[{"role": "system", "content": "You are an intelligent assistant."},
+                {"role": "user", "content": "List the 5 best things to do in " + location}],
+            model="gpt-3.5-turbo"
+        )
 
 @pytest.fixture
 def db():
